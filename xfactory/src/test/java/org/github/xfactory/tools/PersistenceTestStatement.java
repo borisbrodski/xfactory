846020c32d33856a02f22e89d281f0c9076a3c4f
Boris Brodski <brodsky_boris@yahoo.com>
2014-10-21 15:58:59 +0200

    Fix maven build

--- xfactory/src/test/java/org/github/xfactory/tools/PersistenceTestStatement.java
+++ xfactory/src/test/java/org/github/xfactory/tools/PersistenceTestStatement.java
@@ -12,77 +12,83 @@ import org.github.xfactory.XFactory;
 import org.junit.runners.model.Statement;
 
 public class PersistenceTestStatement extends Statement implements InfrastructureProvider {
-	
-	private Statement base;
-	private TestSet testSet;
-	private static Map<TestSet, EntityManagerFactory> entityManagerFactoryMap = new HashMap<TestSet, EntityManagerFactory>();
-	private EntityManager entityManager;
 
-	public PersistenceTestStatement(Statement base, TestSet testSet) {
-		this.base = base;
-		this.testSet = testSet;
-	}
+    private Statement base;
+    private TestSet testSet;
+    private static Map<TestSet, EntityManagerFactory> entityManagerFactoryMap = new HashMap<TestSet, EntityManagerFactory>();
+    private EntityManager entityManager;
 
-	@Override
-	public void evaluate() throws Throwable {
-		boolean ok = false;
-		try {
-			initEntityManager();
-			beginTransaction();
-			XFactory.initTest(this);
-			base.evaluate();
-			ok = true;
-		} finally {
-			XFactory.doneTest();
-			try {
-				rollbackTransaction();
-			} catch (Throwable e) {
-				if (ok) {
-					ok = false;
-					throw e;
-				} else {
-					e.printStackTrace();
-				}
-			} finally {
-				try {
-					closeEntityManager();
-				} catch (Throwable e) {
-					if (ok) {
-						throw e;
-					} else {
-						e.printStackTrace();
-					}
-				}
-			}
-		}
-	}
+    public PersistenceTestStatement(Statement base, TestSet testSet) {
+        this.base = base;
+        this.testSet = testSet;
+    }
 
-	private void beginTransaction() {
-		entityManager.getTransaction().begin();
-	}
+    @Override
+    public void evaluate() throws Throwable {
+        boolean ok = false;
+        try {
+            initEntityManager();
+            beginTransaction();
+            XFactory.initTest(this);
+            base.evaluate();
+            ok = true;
+        } finally {
+            XFactory.doneTest();
+            try {
+                rollbackTransaction();
+            } catch (Throwable e) {
+                if (ok) {
+                    ok = false;
+                    throw e;
+                } else {
+                    e.printStackTrace();
+                }
+            } finally {
+                try {
+                    closeEntityManager();
+                } catch (Throwable e) {
+                    if (ok) {
+                        throw e;
+                    } else {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 
-	private void rollbackTransaction() {
-		entityManager.getTransaction().rollback();
-	}
+    private void beginTransaction() {
+    	if (entityManager != null) {
+    		entityManager.getTransaction().begin();
+    	}
+    }
 
-	private void closeEntityManager() {
-		entityManager.close();
-	}
+    private void rollbackTransaction() {
+    	if (entityManager != null) {
+    		entityManager.getTransaction().rollback();
+    	}
+    }
 
-	private void initEntityManager() {
-		entityManager = getEntityManagerFactory().createEntityManager(); 
-	}
+    private void closeEntityManager() {
+        if (entityManager != null) {
+        	entityManager.close();
+        }
+    }
 
-	private synchronized EntityManagerFactory getEntityManagerFactory() {
-		EntityManagerFactory entityManagerFactory = entityManagerFactoryMap.get(testSet);
-		if (entityManagerFactory == null) {
-			entityManagerFactory = Persistence.createEntityManagerFactory("testset1");
-			entityManagerFactoryMap.put(testSet, entityManagerFactory);
-		}
-		return entityManagerFactory;
-	}
+    private void initEntityManager() {
+        entityManager = getEntityManagerFactory().createEntityManager();
+    }
 
-	public EntityManager getEntityManager() {
-		return entityManager;
-	}
+    private synchronized EntityManagerFactory getEntityManagerFactory() {
+        EntityManagerFactory entityManagerFactory = entityManagerFactoryMap.get(testSet);
+        if (entityManagerFactory == null) {
+            entityManagerFactory = Persistence.createEntityManagerFactory("testset1");
+            entityManagerFactoryMap.put(testSet, entityManagerFactory);
+        }
+        return entityManagerFactory;
+    }
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
 }
